7181bc5375638a432ad5328a43afb386c6b68d7a
MohauwaMorena <justicehlomeli@gmail.com>
2024-04-18 16:49:57 +0200

    Thread added

--- Display.java
+++ Display.java
@@ -2,11 +2,48 @@ import java.awt.Canvas;
 
 import javax.swing.JFrame;
 
-public class Display extends Canvas{
+public class Display extends Canvas implements Runnable{
 	
 	static int WIDTH = 800;
 	static int HEIGHT = 600;
 	final static String title = "More Fire ver 1.1";
+
+	Thread thread;
+	static boolean running = false;
+
+
+	public void run(){
+
+	}
+
+	public void start(){
+		if(running == true){
+			return;
+		}
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+		System.out.println("Game is running");
+
+	}
+
+	public void stop(){
+		if(running == false){
+			return;
+		}
+
+		running = false;
+		try{
+			thread.join();
+		}catch(Exception e){
+			e.printStackTrace();
+			System.exit(0);
+		}
+		System.out.println("Game has ended");
+
+	}
+
+
 
 	public static void main(String[]args){
 		Display game = new Display();
@@ -20,6 +57,8 @@ public class Display extends Canvas{
 		frame.setLocationRelativeTo(null);
 
 		frame.setVisible(true);
+
+		game.start();
 
 
 	}
